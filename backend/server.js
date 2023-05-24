30bff25c7cfe1b888eeec4e15a5d8ca8d3e6cad5
matt <mat@unc.edu>
2023-05-23 20:55:09 -0400

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,23 @@ app.use(express.json());
 app.post("/get-gf-response", (req, res) => {
   const messageContent = req.body.messageContent;
 
-  // Process the messageContent and generate a response
-  const gfResponse = "Hey Cutie";
+  // Process the messageContent and send to LLM for response
+  // send the response back to client
 
+  const gfResponse = "Hey Cutie";
+  res.json({ gfResponse });
+});
+
+app.post("/get-detailed-gf-response", (req, res) => {
+  const messageContent = req.body.messageContent;
+
+  // Process the messageContent
+  // send to LLM instance for response (query for vector DB)
+  // get response from LLM instance and query Pinecone for closest vector embedding
+  // get text response from Pinecone and send with messageContent to LLM again for final response
+  // send the response back to client
+
+  const gfResponse = "Hey Cutie";
   res.json({ gfResponse });
 });
 
